e0ba2194fcbcd343602dc42603a4abcb3dcdba0e
Nathan Vaughn <nathanvaughn06@gmail.com>
2025-11-14 08:18:12 -0600

    Added backbone structure of dungeon generator class

---
+++ project/Dungeon/dungeon_generator.py
@@ -0,0 +1,37 @@
+from .room import Room
+import random
+
+class DungeonGenerator:
+    def __init__(self, width=10, height=10) :
+        self.width = width
+        self.height = height
+        self.rooms = []
+
+    def generate_new_floor(self):
+        self.rooms = []
+        self._generate_rooms()
+        self._connect_rooms()
+        self._place_entities()
+
+    def _generate_rooms(self) :
+        #TODO optimize with queue implementation
+        for i in range(25) :
+            if Room().is_start == True :
+                self.rooms[1].equals(Room())
+            else :
+                self.rooms.append(Room())
+
+    def _connect_rooms(self) :
+        pass #TODO create graph of rooms
+
+    def _place_entities(self) :
+        pass #TODO
+
+    def get_start_room(self) :
+        #TODO
+        return self.rooms[0] if self.rooms else None
+    
+    def update(self, dt) :
+        for room in self.rooms:
+            room.update(dt)
+    
